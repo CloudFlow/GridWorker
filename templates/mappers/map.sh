8f68fbc2af0367241243de6e02a66d7c297e64ce
André Schneider <andre.schneider@eas.iis.fraunhofer.de>
2016-03-22 21:09:31 +0100

    map.sh added

---
+++ templates/mappers/map.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+
+log ()
+{
+  logTimestamp=`date --rfc-3339=seconds`
+  logComponent="gridworker:mapper:script:"
+  logMessage=$1
+  echo "[$logTimestamp] $logComponent $logMessage" >> ${_LOGFILE}
+}
+
+
+check ()
+{
+  log "checked"
+}
+
+abort ()
+{
+  log "aborted"
+}
+
+
+
+notify ()
+{
+  log "notification $1 received"
+}
+
+
+map ()
+{
+  // call your application here
+}
+
+
+${_COMMAND}
+
+exit 0
+
+
